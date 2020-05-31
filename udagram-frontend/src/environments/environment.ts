e49ea6470ad7ea00beb6ad237f59caf0648b49c4
juan <juadel@hotmail.com>
2020-05-31 00:50:16 -0600

    Updated APIhost and configmap

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a2e17ed9876a749b4b93881f4f972659-2079463183.ca-central-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
